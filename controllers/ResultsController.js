cefa12361994266506b3ff06d4df42699b8859c9
davidvorona <davidavorona@gmail.com>
2016-07-21 22:41:43 -0700

    testing more git stuff

--- controllers/ResultsController.js
+++ controllers/ResultsController.js
@@ -5,6 +5,8 @@ angular
   ]);
   .controller('ResultsController', resultsController);
 
+// need to make an $http request to the mongodb and run that data through the resultsController
+
 function resultsController($scope) {
   
 }
