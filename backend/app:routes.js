afe145e66fefee2339293e38d65ac4dc75ba8aec
Shray bansal <bansalshray@gmail.com>
2016-07-22 20:12:07 -0700

    finished basic question RESTful

--- backend/app:routes.js
+++ backend/app:routes.js
@@ -4,6 +4,8 @@ var http = require('http');
 var path = require('path');
 
 var UserController = require('./UserController')
+var QuestionController = require('./QuestionController')
+
 module.exports = function(app) {
 //server routes/ api calls/ authentication routes
 
@@ -14,8 +16,12 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname + '/../client/employer.html'));
 });
 
-app.get('/question/:fName:lName', function(req, res){
-  res.sendFile(path.join(__dirname + '/../client/employer.html'));
+app.get('/question/:fName/:lName', QuestionController.chooseUser, function(req, res){
+	res.render()
+});
+
+app.get('*', function(req, res){
+  res.send('what???', 404);
 });
 
 
